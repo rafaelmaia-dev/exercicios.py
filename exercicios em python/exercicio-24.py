6fdf0e6d9c4dc536a88e6885b00e684211465b45
Rafael Neto Maia <rafaelnetomaiar@gmail.com>
2025-05-27 20:14:58 -0300

    exercícios 24 e 25
    novos exercícios adicionados

---
+++ exercicios em python/exercicio-24.py
@@ -0,0 +1,2 @@
+cidade = str(input('Digite o nome de uma cidade: ')).strip().title()
+print('Santo' in cidade)
---
+++ exercicios em python/exercicio-25.py
@@ -0,0 +1,2 @@
+nome = str(input('Digite o seu nome completo: ')).strip().title()
+print('Silva' in nome)
